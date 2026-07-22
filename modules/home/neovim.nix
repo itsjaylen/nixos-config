5d7a83763a5b16579d7325d7b3a26a4f849e39ec
Jaylen <jaylen@localhost>
2026-07-22 19:27:56 -0400

    track neovim module

---
+++ modules/home/neovim.nix
@@ -0,0 +1,29 @@
+# modules/home/neovim.nix
+{ pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  # Dependencies required by NvChad, Mason, and treesitter
+  home.packages = with pkgs; [
+    git
+    gcc
+    ripgrep
+    fd
+    nodejs
+    tree-sitter
+  ];
+
+  # Fetch NvChad Starter config to ~/.config/nvim if you want fresh NvChad v2.5
+  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
+    owner = "NvChad";
+    repo = "NvChad";
+    rev = "v2.5";
+    hash = "sha256-N345B0PqL026SAnlR1kQk3yB9AAnfH4Uq12xO7mQ2Bw=";
+  };
+}
