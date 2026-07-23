e3eff87f5ae0bd300146743dcced2c94c0f731c9
Jaylen <jaylen@localhost>
2026-07-22 22:00:36 -0400

    fix(flake): correctly nest noctalia-qs inside inputs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:youwen5/zen-browser-flake";
-    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
     
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # 1. Add Home Manager input
+    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,19 +21,17 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Moved noctalia-qs correctly inside the inputs block
     noctalia-qs = {
       url = "github:noctalia-dev/noctalia-qs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  # Added noctalia-qs to the outputs arguments list
-  outputs = { self, nixpkgs, home-manager, spicetify-nix, millennium, noctalia-qs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, spicetify-nix, millennium, zen-browser, noctalia-qs, ... }@inputs: {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
-      # Pass inputs to NixOS system modules (core.nix, desktop.nix, etc.)
+      # Pass inputs to NixOS system modules
       specialArgs = { inherit inputs; };
 
       modules = [
@@ -41,13 +42,13 @@
         ./modules/audio.nix
         ./modules/gaming.nix
 
-        # 2. Add Home Manager as a module
+        # Add Home Manager as a module
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # Pass inputs to Home Manager modules (home.nix)
+          # Pass inputs to Home Manager modules
           home-manager.extraSpecialArgs = { inherit inputs; };
 
           # Pass user config to Home Manager
