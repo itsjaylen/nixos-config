d1c2ba492796e882e91a55e759becd70edc69a04
Jaylen <jaylen@localhost>
2026-07-22 22:00:46 -0400

    fix(flake): correctly nest noctalia-qs inside inputs

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -13,7 +13,7 @@
       enable = true;
       extraPortals = [ 
         pkgs.kdePackages.xdg-desktop-portal-kde
-  pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gtk
       ];
       xdgOpenUsePortal = true;
     };
