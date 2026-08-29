97f41f0ae1b9c8aa1ab6ea1e65d22997590ded5a
itsjaylen <bossjaylen145@gmail.com>
2026-08-29 12:57:55 -0400

    Update default.nix

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -16,23 +16,21 @@
   security.pam.services.swaylock.kwallet.enable = lib.mkForce false;
   environment.systemPackages = lib.mkForce [];
 
-  # Allow the wheel group to use sudo without a password (giving jaylen root-level control)
+  # Allow the wheel group to use sudo without a password
   security.sudo.wheelNeedsPassword = false;
 
   users.users = {
     "${username}" = {
-      isNormalUser = true;
-      description = "Jaylen";
-      extraGroups = [
+      extraGroups = lib.mkForce [
         "wheel"
         "networkmanager"
         "minecraft"
       ];
-      shell = pkgs.fish;
-      openssh.authorizedKeys.keys = [
+      shell = lib.mkForce pkgs.fish;
+      openssh.authorizedKeys.keys = lib.mkForce [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCeOD9pGd9bBTlxxorOmjM23pObPLDFyjGIbgvOz+BqdLIp1ngsNB/SyQSAPoKzhpIn5mQKX414SjPfZ9e83j4PUK+7AzWey/yIn9ul9FnUmIluVNIHr3W9uIFVaZGHQBZzmzb8tqNRHNp+EnZhekJKmiZtgUMong2qh61ssUoozbbTO6+YRoZJakNZhwWeIXZD0bXEv5x0UEjv8GF0na+OOC2RTfCvNUxAvlqZmxr365tBgrvB6kpekpFKHvEhziWRCJqzdyG9mKGRnCZrVnQCXiFhauRmlxKnPngiWE2q0fM++VVUPRsT0MAvslYiLY+VxQS78okCJ9nB5YRA25CaJhR/O+o47AQsNTtjZZbt0uAnTCB537x0P08rf8qZViaa+zaYeVouHfIhbR+9f7ZCeMdHbyKEz+7yLSR/Li5Y6yo79khiBGNYZucPJuDPXpB486dwpVnnNvURXluTtNsTXcwXtyAYt8luUtkmggL8JLR1MiQnYxh96npOuzPuRQvBaDbWVelMhaEZLTpHnLFsAszZ/12zzLTO7xK6Srn0FKI36tBsj2t4CKJlm7g+orG73i8E7AN4TZZzh062x1wxUjRy7D7ibM21G+SdxnrfmOVsOxfKvVjLeHdEOi0DKHElzkXD+re5HElUAesBmBrMv2mTeYRZUBUTh/c3/YY2qw== bossjaylen145@gmail.com"
       ];
-      linger = true;
+      linger = lib.mkForce true;
     };
 
     minecraft = {
