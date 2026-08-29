fff5cf76020a44d2ef0034a6c5876ec306f02aa4
itsjaylen <bossjaylen145@gmail.com>
2026-08-28 20:31:57 -0400

    Update default.nix

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, host, ... }: {
+{ config, pkgs, lib, username, ... }: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/core/bootloader.nix
@@ -9,12 +9,40 @@
     ../../modules/core/security.nix
   ];
 
+  # Server-specific overrides
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
 
-  # Disable GUI wallet services not needed on a headless server
   security.pam.services.login.kwallet.enable = lib.mkForce false;
   security.pam.services.swaylock.kwallet.enable = lib.mkForce false;
-
-  # Remove the KDE wallet system packages introduced by security.nix
   environment.systemPackages = lib.mkForce [];
+
+  # Allow the wheel group to use sudo without a password (giving jaylen root-level control)
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users = {
+    "${username}" = {
+      isNormalUser = true;
+      description = "Jaylen";
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "minecraft"
+      ];
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCeOD9pGd9bBTlxxorOmjM23pObPLDFyjGIbgvOz+BqdLIp1ngsNB/SyQSAPoKzhpIn5mQKX414SjPfZ9e83j4PUK+7AzWey/yIn9ul9FnUmIluVNIHr3W9uIFVaZGHQBZzmzb8tqNRHNp+EnZhekJKmiZtgUMong2qh61ssUoozbbTO6+YRoZJakNZhwWeIXZD0bXEv5x0UEjv8GF0na+OOC2RTfCvNUxAvlqZmxr365tBgrvB6kpekpFKHvEhziWRCJqzdyG9mKGRnCZrVnQCXiFhauRmlxKnPngiWE2q0fM++VVUPRsT0MAvslYiLY+VxQS78okCJ9nB5YRA25CaJhR/O+o47AQsNTtjZZbt0uAnTCB537x0P08rf8qZViaa+zaYeVouHfIhbR+9f7ZCeMdHbyKEz+7yLSR/Li5Y6yo79khiBGNYZucPJuDPXpB486dwpVnnNvURXluTtNsTXcwXtyAYt8luUtkmggL8JLR1MiQnYxh96npOuzPuRQvBaDbWVelMhaEZLTpHnLFsAszZ/12zzLTO7xK6Srn0FKI36tBsj2t4CKJlm7g+orG73i8E7AN4TZZzh062x1wxUjRy7D7ibM21G+SdxnrfmOVsOxfKvVjLeHdEOi0DKHElzkXD+re5HElUAesBmBrMv2mTeYRZUBUTh/c3/YY2qw== bossjaylen145@gmail.com"
+      ];
+      linger = true;
+    };
+
+    minecraft = {
+      isSystemUser = true;
+      group = "minecraft";
+      home = "/var/lib/minecraft";
+      createHome = true;
+      description = "Minecraft daemon user";
+    };
+  };
+
+  users.groups.minecraft = {};
 }
