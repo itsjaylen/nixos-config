201995f6edbee3f44bcf76a9802ec8f5f4aebad3
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 15:15:35 -0400

    Update cloudflared.nix

--- hosts/server/services/cloudflared.nix
+++ hosts/server/services/cloudflared.nix
@@ -1,19 +1,27 @@
 { config, pkgs, ... }: {
   sops.secrets.cloudflare-env = {
-    # Put TUNNEL_TOKEN=ey... inside secrets.yaml under key cloudflare-env
+    owner = "cloudflared";
+    group = "cloudflared";
     mode = "0400";
   };
 
-  services.cloudflared = {
-    enable = true;
-    tunnels = {
-      "server-tunnel" = {
-        default = "http_status:404";
-      };
-    };
+  users.users.cloudflared = {
+    group = "cloudflared";
+    isSystemUser = true;
   };
+  users.groups.cloudflared = {};
 
-  systemd.services."cloudflared-tunnel-server-tunnel".serviceConfig = {
-    EnvironmentFile = config.sops.secrets.cloudflare-env.path;
+  systemd.services.cloudflared-tunnel = {
+    description = "Cloudflare Tunnel Daemon";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run";
+      EnvironmentFile = config.sops.secrets.cloudflare-env.path;
+      Restart = "always";
+      User = "cloudflared";
+      Group = "cloudflared";
+    };
   };
 }
