349b76fe81ee1f4af7d1fe5921775a9fc84f7000
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 18:26:17 -0400

    Update grafana.nix

--- hosts/server/services/grafana.nix
+++ hosts/server/services/grafana.nix
@@ -1,44 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    sops
-    age
-    ssh-to-age
-  ];
-
-  sops = {
-    defaultSopsFile = ../../secrets/secrets.yaml;
-    defaultSopsFormat = "yaml";
-
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    age.generateKey = true;
-
-    secrets = {
-      "github_ssh_key" = {
-        owner = "jaylen";
-        group = "users";
-        path = "/home/jaylen/.ssh/id_ed25519";
-        mode = "0600";
-      };
-      "gitea_token" = {
-        owner = "jaylen";
-        group = "users";
-      };
-      "garage_rpc_secret" = { };
-      "garage_s3_access_key" = { };
-      "garage_s3_secret_key" = { };
-      "restic_password" = { };
-      "grafana_admin_password" = {
-        owner = "grafana";
-      };
-      "grafana_secret_key" = {
-        owner = "grafana";
-      };
+  sops.secrets = {
+    "grafana_admin_password" = {
+      owner = "grafana";
+    };
+    "grafana_secret_key" = {
+      owner = "grafana";
     };
   };
 
-  system.activationScripts.ensureSshDir = ''
-    install -d -m 700 -o jaylen -g users /home/jaylen/.ssh
-  '';
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "0.0.0.0";
+        http_port = 3001;
+      };
+      security = {
+        admin_password = "$__file{${config.sops.secrets."grafana_admin_password".path}}";
+        secret_key = "$__file{${config.sops.secrets."grafana_secret_key".path}}";
+      };
+    };
+  };
 }
