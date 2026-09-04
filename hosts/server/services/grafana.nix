ea9fd8fb0189bf7bb9e1c05e326b3fe7ecf60d88
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 18:38:25 -0400

    Update grafana.nix

--- hosts/server/services/grafana.nix
+++ hosts/server/services/grafana.nix
@@ -13,15 +13,20 @@
   services.grafana = {
     enable = true;
     settings = {
+      app_mode = "production";
       server = {
         http_addr = "0.0.0.0";
         http_port = 3001;
         domain = "192.168.50.188";
-        root_url = "http://192.168.50.188:3001/";
+        root_url = "%(protocol)s://%(domain)s:%(http_port)s/";
+        serve_from_sub_path = false;
+        enforce_domain = false;
       };
       security = {
         admin_password = "$__file{${config.sops.secrets."grafana_admin_password".path}}";
         secret_key = "$__file{${config.sops.secrets."grafana_secret_key".path}}";
+        cookie_secure = false;
+        cookie_samesite = "lax";
       };
     };
   };
