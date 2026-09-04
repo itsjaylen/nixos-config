d3ad11f0660db9a18da0c607a4e383ea6db1e383
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 18:18:46 -0400

    Update grafana.nix

--- hosts/server/services/grafana.nix
+++ hosts/server/services/grafana.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: {
   services.grafana = {
     enable = true;
+
+    # Secret file containing environment variables for Grafana
+    secretFile = config.sops.templates."grafana-env".path;
+
     settings = {
       server = {
         http_addr = "0.0.0.0";
@@ -9,12 +13,10 @@
       };
       security = {
         admin_user = "admin";
-        # Pull admin password safely from SOPS
         admin_password = "$__file{${config.sops.secrets."grafana_admin_password".path}}";
       };
     };
 
-    # Declaratively attach Prometheus as the default data source
     provision = {
       enable = true;
       datasources.settings.datasources = [
@@ -27,6 +29,14 @@
         }
       ];
     };
+  };
+
+  # Generate environment variable file for Grafana systemd service
+  sops.templates."grafana-env" = {
+    content = ''
+      GF_SECURITY_SECRET_KEY=${config.sops.placeholder.grafana_secret_key}
+    '';
+    owner = "grafana";
   };
 
   networking.firewall.allowedTCPPorts = [ 3001 ];
