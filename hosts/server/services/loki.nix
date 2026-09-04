3e218333c228b5c95c96fc6e2fd5862acbda0cb4
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 19:32:16 -0400

    Update loki.nix

--- hosts/server/services/loki.nix
+++ hosts/server/services/loki.nix
@@ -31,39 +31,41 @@
   };
 
   services.alloy = {
-      enable = true;
-      extraGroups = [ "systemd-journal" ];
-      configPath = pkgs.writeText "config.alloy" ''
-        loki.relabel "journal" {
-          rule {
-            source_labels = ["__journal__systemd_unit"]
-            target_label  = "unit"
-          }
+    enable = true;
+    configPath = pkgs.writeText "config.alloy" ''
+      loki.relabel "journal" {
+        rule {
+          source_labels = ["__journal__systemd_unit"]
+          target_label  = "unit"
         }
-  
-        discovery.relabel "journal" {
-          targets = []
-  
-          rule {
-            source_labels = ["__journal__systemd_unit"]
-            target_label  = "unit"
-          }
+      }
+
+      discovery.relabel "journal" {
+        targets = []
+
+        rule {
+          source_labels = ["__journal__systemd_unit"]
+          target_label  = "unit"
         }
-  
-        loki.source.journal "journal" {
-          max_age   = "12h"
-          path      = "/var/log/journal"
-          relabel_rules = loki.relabel.journal.rules
-          forward_to = [loki.write.endpoint.receiver]
+      }
+
+      loki.source.journal "journal" {
+        max_age   = "12h"
+        path      = "/var/log/journal"
+        relabel_rules = loki.relabel.journal.rules
+        forward_to = [loki.write.endpoint.receiver]
+      }
+
+      loki.write "endpoint" {
+        endpoint {
+          url = "http://127.0.0.1:3100/loki/api/v1/push"
         }
-  
-        loki.write "endpoint" {
-          endpoint {
-            url = "http://127.0.0.1:3100/loki/api/v1/push"
-          }
-        }
-      '';
-    };
+      }
+    '';
+  };
+
+  # Grant alloy permission to read systemd journal logs
+  systemd.services.alloy.serviceConfig.SupplementaryGroups = [ "systemd-journal" ];
 
   networking.firewall.allowedTCPPorts = [ 3100 ];
 }
