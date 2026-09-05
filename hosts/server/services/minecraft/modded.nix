7a5aae9fa31b64ed7e0f51db8715935f4234c49b
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 14:31:43 -0400

    Update modded.nix

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -10,5 +10,15 @@
       server-port = 25566;
       motd = "Modded";
     };
+
+    # Declaratively add mods from Modrinth
+    files = {
+      "mods/jade.jar" = {
+        value = pkgs.fetchModrinthMod {
+          mod = "jade";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+      };
+    };
   };
 }
