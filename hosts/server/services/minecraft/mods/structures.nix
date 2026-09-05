b76499d13b909f0d0ecfdc665483392505e46c26
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 19:33:05 -0400

    added files

---
+++ hosts/server/services/minecraft/mods/structures.nix
@@ -0,0 +1,18 @@
+{ pkgs }:
+
+let
+  mod = name: url: sha512: {
+    name = "mods/${name}.jar";
+    value = pkgs.fetchurl { inherit url sha512; };
+  };
+in
+[
+  # Structory Family
+  (mod "Structory" 
+    "https://cdn.modrinth.com/data/aKCwCJlY/versions/TUbwu7eG/Structory_26.2_v1.3.7.jar" 
+    "4178a15f32f6ed3d59a7eb2222a8e3f539efd7b54d8ffb790cbf1dfe5c54c6ce6ec33220e148424109c6360c4c6f081b9fbebfe74745fe3c8f05619a06ccb9ec")
+
+  (mod "StructoryTowers" 
+    "https://cdn.modrinth.com/data/j3FONRYr/versions/ziO4YIv1/Structory_Towers_26.2_v1.0.17.jar" 
+    "74b1f29439e2946ef9b3f9d8d7915bc112239d5009587c7817c4f45bee7e7a48906ffcddab67f0078fe82e996093657ab874a5a0498ccc045dda4bd562bc7702")
+]
