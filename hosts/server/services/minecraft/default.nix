6fe17707004f9ae0bf3e19a76cf2790a6cbbd08b
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 22:59:38 -0400

    Update default.nix

--- hosts/server/services/minecraft/default.nix
+++ hosts/server/services/minecraft/default.nix
@@ -9,14 +9,15 @@
     after = [ "network.target" ];
 
     serviceConfig = {
-      Type = "simple";
+      Type = "forking";
       User = "minecraft";
       Group = "minecraft";
       WorkingDirectory = "/var/lib/minecraft";
       Environment = "HOME=/var/lib/minecraft";
 
+      # Use -d for detached mode so tmux doesn't look for an active terminal
       ExecStart = ''
-        ${pkgs.tmux}/bin/tmux new-session -s minecraft \
+        ${pkgs.tmux}/bin/tmux new-session -d -s minecraft \
         "${pkgs.temurin-bin}/bin/java -Xmx2G -Xms2G -jar /var/lib/minecraft/versions/26.2/server-26.2.jar nogui"
       '';
 
