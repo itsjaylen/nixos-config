1965b1cdd500bc55f72f950d7ffb9c13f757025b
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 23:00:29 -0400

    Update default.nix

--- hosts/server/services/minecraft/default.nix
+++ hosts/server/services/minecraft/default.nix
@@ -17,9 +17,9 @@
 
       # Use -d for detached mode so tmux doesn't look for an active terminal
       ExecStart = ''
-        ${pkgs.tmux}/bin/tmux new-session -d -s minecraft \
-        "${pkgs.temurin-bin}/bin/java -Xmx2G -Xms2G -jar /var/lib/minecraft/versions/26.2/server-26.2.jar nogui"
-      '';
+              ${pkgs.tmux}/bin/tmux new-session -d -s minecraft \
+              "${pkgs.temurin-bin}/bin/java -Xmx2G -Xms2G -jar /var/lib/minecraft/versions/26.2/server-26.2.jar nogui"
+            '';
 
       ExecStop = "${pkgs.tmux}/bin/tmux send-keys -t minecraft stop Enter";
       TimeoutStopSec = 60;
