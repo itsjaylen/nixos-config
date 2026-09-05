a604cbf9258e2b2d441850f0ea2c1b16501bb669
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 15:29:14 -0400

    Update modded.nix

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -9,16 +9,38 @@ in
 {
   services.minecraft-servers.servers.youer = {
     enable = true;
-    package = pkgs.jdk21;
-    jvmOpts = "-Xms2G -Xmx2G -jar ${youerServerJar} nogui";
     
-    # Explicitly match user/group to avoid root socket ownership mismatch
-    user = "minecraft";
-    group = "minecraft";
-  
+    # Use standard openjdk package
+    package = pkgs.jdk21;
+
+    # Explicitly force the service to run as the correct user/group so sockets match
+    # (nix-minecraft defaults to the minecraft user)
+    # Pass arguments properly
+    jvmOpts = "-Xms2G -Xmx2G -jar ${youerServerJar} nogui";
+
     serverProperties = {
       server-port = 25566;
       motd = "Youer NeoForge + Paper Hybrid";
+    };
+
+    symlinks = {
+      "mods/FerriteCore.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/uXXizFIs/versions/LtVvw4uS/ferritecore-9.0.0-neoforge.jar";
+        sha512 = "e96a99ac5539f56a1f4cd109d62b668ebd5283f0068491ede956f52e67023beba7abe2e40021499352ffc41ead950bebcabce7792352249a1b45c5dccb3cf99c";
+      };
+
+      "mods/Spark.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/l6YH9Als/versions/DdMsOH3O/spark-1.10.173-neoforge.jar";
+        sha512 = "f40b72761c2137debe90c836a32918e4e3aa2629db4b50e9b78bdcacdbe6e484682ba7e11535bee7fcf581abe944948dde48dda37ee45d3966a5d7e450191173";
+      };
+    };
+  };
+  
+  # Ensure systemd service explicitly runs as the minecraft user to fix socket ownership
+  systemd.services.minecraft-server-youer = {
+    serviceConfig = {
+      User = "minecraft";
+      Group = "minecraft";
     };
   };
 }
