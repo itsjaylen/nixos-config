c9f070abf3b97697b98051eb823f9a3008cd8dae
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 14:22:36 -0400

    fix modded

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -3,7 +3,7 @@
 {
   services.minecraft-servers.servers.neoforge = {
     enable = true;
-    package = pkgs.neoforgeServers.neoforge;
+    package = pkgs.neoforgeServers.neoforge-26_2;
     jvmOpts = "-Xms2G -Xmx2G";
 
     serverProperties = {
