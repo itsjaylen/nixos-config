8cede120a172f230b73c9a248327735f2c820eed
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 19:00:19 -0400

    reverted to test

--- hosts/server/services/minecraft/mods/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-
-let
-  plugins = import ./plugins.nix { inherit pkgs; };
-  worldgen = import ./worldgen.nix { inherit pkgs; };
-  structures = import ./structures.nix { inherit pkgs; };
-in
-builtins.listToAttrs (plugins ++ worldgen ++ structures)
