24c1add1084ed05191ed49101333c1616ca5bf27
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 21:46:06 -0400

    Update qbittorrent.nix

--- hosts/server/services/qbittorrent.nix
+++ hosts/server/services/qbittorrent.nix
@@ -48,9 +48,8 @@
                 WebUI = {
                   Address = "*";
                   LocalHostAuth = false;
+                  BypassLocalAuth = true;
                   Username = "admin";
-                  # This is the official PBKDF2 hash for the password: adminadmin
-                  Password_PBKDF2 = "@ByteArray(ARQ77eY1NUZaQsuDHbIMCA==:0WMRkYTUWVT9wVvdDtHAjU9b3b7uB8NR1Gur2hmQCvCDpm39Q+PsJRJPaCU51dEiz+dTzh8qbPsL8WkFljQYFQ==)";
                 };
               };
             };
