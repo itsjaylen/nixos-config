757e1458daa613e2008496dc39a39c8974e908ba
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 23:13:27 -0400

    Update default.nix

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, lib, username, inputs, ... }: {
+{ config, pkgs, lib, username, inputs, ... }:
+
+{
   imports = [
     ./hardware-configuration.nix
     ./services
@@ -16,24 +18,33 @@
     "${username}" = {
       isNormalUser = true;
       description = "${username}";
+
       extraGroups = [
         "wheel"
         "networkmanager"
         "minecraft"
       ];
+
       shell = pkgs.fish;
+
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCHsjBvdZ7/oqWa0YK1dD6NLSgr1d+eJk9YnrD3tAGj bossjaylen145@gmail.com"
       ];
+
       linger = true;
     };
-
-  programs.fish.enable = true;
-  programs.fish.shellAliases = {
-    restic-garage = "sudo env (sudo cat /run/secrets/rendered/restic-env | string match -v '^#*') restic -r s3:http://127.0.0.1:3900/my-bucket";
   };
 
-  nix.settings.allowed-users = [ "@wheel" ];
+  programs.fish.enable = true;
+
+  programs.fish.shellAliases = {
+    restic-garage =
+      "sudo env (sudo cat /run/secrets/rendered/restic-env | string match -v '^#*') restic -r s3:http://127.0.0.1:3900/my-bucket";
+  };
+
+  nix.settings.allowed-users = [
+    "@wheel"
+  ];
 
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.tmp.useTmpfs = lib.mkForce false;
