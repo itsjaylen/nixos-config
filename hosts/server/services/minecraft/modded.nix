e790ed31f3721edd00146c93738d6d48c928daba
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 14:03:13 -0400

    Update modded.nix

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }:
 
 {
-  services.minecraft-servers.servers.modded = {
-    enable = true; 
-    eula = true; # <-- Missing: Required for the server to run
-    package = pkgs.neoforgeServers.neoforge;
-    jvmOpts = "-Xms4G -Xmx4G";
+  services.minecraft-servers = {
+    enable = true;
+    eula = true; # <-- Must be defined here globally
 
-    serverProperties = {
-      server-port = 25566;
-      motd = "Modded Chaos";
+    servers.modded = {
+      enable = true; 
+      package = pkgs.neoforgeServers.neoforge;
+      jvmOpts = "-Xms4G -Xmx4G";
+
+      serverProperties = {
+        server-port = 25566;
+        motd = "Modded Chaos";
+      };
     };
   };
 }
