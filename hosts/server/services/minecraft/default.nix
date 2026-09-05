5e27742621fb1b47b264e78db7b5bbb6fd64abbd
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 17:21:01 -0400

    Update default.nix

--- hosts/server/services/minecraft/default.nix
+++ hosts/server/services/minecraft/default.nix
@@ -11,14 +11,6 @@
     eula = true;
   };
 
-  # Automatically open firewall ports
-  networking.firewall.allowedTCPPorts = [ 25565 25566 25567 8100 ];
-
-  # Systemd override for the neoforge minecraft server
-  systemd.services."minecraft-neoforge" = {
-    serviceConfig = {
-      Restart = "always";
-      RestartSec = "10s"; # Waits 10 seconds before attempting a restart after a crash
-    };
-  };
+  # Automatically open firewall ports if defined, or manage them globally
+  networking.firewall.allowedTCPPorts = [ 25565 25566 25567 8100];
 }
