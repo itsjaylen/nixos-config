28d484dd64ffbe9bf55dcc4cf993b27df40fa1ff
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 17:56:47 -0400

    added Structory and Structory towers

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -83,7 +83,16 @@ in
                     url = "https://cdn.modrinth.com/data/ZVzW5oNS/versions/vPwuqZ5y/Incendium_26.2_v5.5.1.jar";
                     sha512 = "202e1f23c64c37330d3ec0bfd80331355d15cc8d01f151edc4dabfa8ce719650d10f468df5bf523a93461d78c3660a2cdedbc1037fb08396c13f22b6457352f0";
                   };
-                  
+
+            "mods/Structory" = pkgs.fetchurl {
+                    url = "https://cdn.modrinth.com/data/aKCwCJlY/versions/TUbwu7eG/Structory_26.2_v1.3.7.jar";
+                    sha512 = "4178a15f32f6ed3d59a7eb2222a8e3f539efd7b54d8ffb790cbf1dfe5c54c6ce6ec33220e148424109c6360c4c6f081b9fbebfe74745fe3c8f05619a06ccb9ec";
+                  };
+
+            "mods/Structory:Towers" = pkgs.fetchurl {
+                    url = "https://cdn.modrinth.com/data/j3FONRYr/versions/ziO4YIv1/Structory_Towers_26.2_v1.0.17.jar";
+                    sha512 = "74b1f29439e2946ef9b3f9d8d7915bc112239d5009587c7817c4f45bee7e7a48906ffcddab67f0078fe82e996093657ab874a5a0498ccc045dda4bd562bc7702";
+                  };
         };
   };
 }
