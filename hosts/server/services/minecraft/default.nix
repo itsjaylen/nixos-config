a5306dcfe5165823a57816d31886cbb96ce4de30
itsjaylen <bossjaylen145@gmail.com>
2026-09-04 23:50:01 -0400

    added temurin

--- hosts/server/services/minecraft/default.nix
+++ hosts/server/services/minecraft/default.nix
@@ -1,28 +1,25 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.minecraft-servers = {
-    enable = true;
-    eula = true;
-
-    servers.minecraft = {
-      enable = true;
-
-      # Use the package provided by nix-minecraft.
-      package = pkgs.paperServers.paper-26_2;
-
-      jvmOpts = "-Xms2G -Xmx2G";
-
-      serverProperties = {
-        server-port = 25565;
-        motd = "Jaylen's Minecraft Server";
-        online-mode = true;
-        enable-command-block = true;
-        view-distance = 10;
-        simulation-distance = 10;
+  servers.minecraft = {
+        enable = true;
+        
+        # Use Paper and override its JRE package to Eclipse Temurin (pkgs.temurin-bin)
+        package = (pkgs.paperServers.paper-26_2.override {
+          jre = pkgs.temurin-bin;
+        });
+  
+        jvmOpts = "-Xms2G -Xmx2G";
+  
+        serverProperties = {
+          server-port = 25565;
+          motd = "Jaylen's Minecraft Server";
+          online-mode = true;
+          enable-command-block = true;
+          view-distance = 10;
+          simulation-distance = 10;
+        };
       };
-    };
-  };
 
   networking.firewall.allowedTCPPorts = [
     25565
