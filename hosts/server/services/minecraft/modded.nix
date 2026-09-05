270fad65a44c35e2c526844a0bd88671f8d790d0
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 19:32:26 -0400

    changed stutured

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -23,76 +23,7 @@ in
       enforce-secure-profile = false;
     };
 
-    symlinks = {
-          "mods/Spark.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/l6YH9Als/versions/DdMsOH3O/spark-1.10.173-neoforge.jar";
-            sha512 = "f40b72761c2137debe90c836a32918e4e3aa2629db4b50e9b78bdcacdbe6e484682ba7e11535bee7fcf581abe944948dde48dda37ee45d3966a5d7e450191173";
-          };
-    
-          "plugins/WorldEdit.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/1u6JkXh5/versions/F5ea2ov3/worldedit-bukkit-7.4.5.jar";
-            sha512 = "sha512-o4NJL6xr+01Dolffp7X8B2quUDpxFRtGPeT+gObz1fwRIJ6vQJe6oRXz/r8K3EDKCh7Noie4Q5tCnQpLo6Y6Tw==";
-          };
-    
-          "plugins/EssentialsX.jar" = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/hXiIvTyT/versions/nY6VN1XH/EssentialsX-2.22.0.jar";
-            sha512 = "sha512-Ry7PcZJIAXI2Q8puH5kxKX3lqgh0pUT4pxni4vfIGvge97y9OLbkPwJgqdrmq+Qsl5B+cOJDDjJv+M/xLr1hzw==";
-          };
-    
-          "plugins/Essentials/config.yml" = pkgs.runCommand "essentials-config.yml" {
-            src = pkgs.fetchurl {
-              url = "https://raw.githubusercontent.com/EssentialsX/Essentials/2.22.0/Essentials/src/main/resources/config.yml";
-              sha256 = "sha256-M4As04JRLI8dHUtBBY8gMBR8RtbxRaIRBMlQioG7MhA=";
-            };
-          } ''
-            substitute $src $out \
-              --replace "unsafe-enchantments: false" "unsafe-enchantments: true"
-          '';
-
-          "plugins/BlueMap.jar" = pkgs.fetchurl {
-                  url = "https://cdn.modrinth.com/data/swbUV1cr/versions/K5U1ASjn/bluemap-5.23-paper.jar";
-                  sha512 = "sha512-qHiT11FkBd+kXhK0R73r0E7ktCP/TRCWQgB5bZDla/eJ7a8gPsMBP5JigGl7EpD7zXJUT9GSkQxF+IzlW5eLsQ==";
-                };
-
-          "plugins/BlueMap/core.conf" = pkgs.writeText "core.conf" ''
-                      # BlueMap Core Config
-                      accept-download: true
-                    '';
-
-          "plugins/FreedomChat.jar" = pkgs.fetchurl {
-                  url = "https://cdn.modrinth.com/data/MubyTbnA/versions/Pqu2VLTB/FreedomChat-Paper-1.7.9.jar";
-                  sha512 = "94c97628e4b13762d9ba029fa1130300dccd63d31f15b5f29e96fc0586abd7392315fed8f51c772ba7e14fc4738a31d7bbd3c8c106d9929d378334ffe65c96d2";
-                };
-          
-            "mods/Terralith.jar" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/lqrGyTjO/Terralith_26.2_v2.6.4_Neoforge.jar";
-                    sha512 = "b66ade8d34cb61af8174eac4af9905d741a456fdd4c32f1674d106a47094ae560cd9652cf4904ebff591c931134dc3d307fbfa0904503d202e1048289b6f8d3b";
-                  };
-
-            "mods/lithostitched.jar" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/XaDC71GB/versions/uJERiR1V/lithostitched-1.8.0%2Bbeta3-neoforge-26.2.jar";
-                    sha512 = "9a8b6b38c58076d601c25b03eeb3f398385467ffd5029ebc7b6f403289ad150fd4a0b550de0bd3bfa0af49f308c2eb633743677630821c10849318b42e7dd229";
-                  };
-
-            "mods/Nullscape.jar" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/LPjGiSO4/versions/lpsdO6Sg/Nullscape_26.2_v1.2.20.jar";
-                    sha512 = "8c8f5ec050fb038da653c1023d2bbaf01cf0a88354d8081dbf99513375a64ebc18c669650d017c938ab4c4314c47f8b706422d8df43f4652c6c85460a63b6f01";
-                  };
-
-            "mods/Incendium.jar" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/ZVzW5oNS/versions/vPwuqZ5y/Incendium_26.2_v5.5.1.jar";
-                    sha512 = "202e1f23c64c37330d3ec0bfd80331355d15cc8d01f151edc4dabfa8ce719650d10f468df5bf523a93461d78c3660a2cdedbc1037fb08396c13f22b6457352f0";
-                  };
-
-            "mods/Structory" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/aKCwCJlY/versions/TUbwu7eG/Structory_26.2_v1.3.7.jar";
-                    sha512 = "4178a15f32f6ed3d59a7eb2222a8e3f539efd7b54d8ffb790cbf1dfe5c54c6ce6ec33220e148424109c6360c4c6f081b9fbebfe74745fe3c8f05619a06ccb9ec";
-                  };
-
-            "mods/Structory:Towers" = pkgs.fetchurl {
-                    url = "https://cdn.modrinth.com/data/j3FONRYr/versions/ziO4YIv1/Structory_Towers_26.2_v1.0.17.jar";
-                    sha512 = "74b1f29439e2946ef9b3f9d8d7915bc112239d5009587c7817c4f45bee7e7a48906ffcddab67f0078fe82e996093657ab874a5a0498ccc045dda4bd562bc7702";
-                  };
-        };
+    # Imports the master mod router directory
+    symlinks = import ./mods { inherit pkgs; };
   };
 }
