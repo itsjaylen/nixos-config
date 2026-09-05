1229c61235ab2654af723f2cd84ddfefac526f8b
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 14:26:50 -0400

    Update modded.nix

--- hosts/server/services/minecraft/modded.nix
+++ hosts/server/services/minecraft/modded.nix
@@ -4,8 +4,9 @@
   services.minecraft-servers.servers.neoforge = {
     enable = true;
     package = pkgs.neoforgeServers.neoforge-26_2;
+    # Force Java 21 instead of the system default
+    java.package = pkgs.jdk21; 
     jvmOpts = "-Xms2G -Xmx2G";
-
     serverProperties = {
       server-port = 25566;
       motd = "Modded";
