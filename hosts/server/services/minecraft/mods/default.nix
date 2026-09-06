a937a3256b466c528d0a7315b4cca5dec9de49fa
itsjaylen <bossjaylen145@gmail.com>
2026-09-05 21:06:04 -0400

    added gameplay.nix with moreenchants and crazyenchantments

--- hosts/server/services/minecraft/mods/default.nix
+++ hosts/server/services/minecraft/mods/default.nix
@@ -5,5 +5,6 @@ let
   worldgen = import ./worldgen.nix { inherit pkgs; };
   structures = import ./structures.nix { inherit pkgs; };
   utility = import ./utility.nix { inherit pkgs; };
+  gameplay = import ./gameplay.nix { inherit pkgs; };
 in
-builtins.listToAttrs (plugins ++ worldgen ++ structures ++ utility)
+builtins.listToAttrs (plugins ++ worldgen ++ structures ++ utility ++ gameplay)
