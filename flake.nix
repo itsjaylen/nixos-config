c2914e5333f3849730905385d8e27958e092a4a6
itsjaylen <bossjaylen145@gmail.com>
2026-09-09 16:40:56 -0400

    fix flake

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
     };
 
     rustlog = {
-      url = "github:Julia-Roman/rustlog";
+      url = "git+https://github.com/Julia-Roman/rustlog?submodules=1";
       flake = false;
     };
 
--- pkgs/rustlog/default.nix
+++ pkgs/rustlog/default.nix
@@ -5,22 +5,13 @@
   nodejs,
   pkg-config,
   openssl,
-  fetchFromGitHub,
 }:
 
 rustPlatform.buildRustPackage {
   pname = "rustlog";
   version = "unstable-2026-09-09";
 
-  # Fetch from GitHub directly with submodules enabled instead of using inputs.rustlog directly,
-  # or use fetchGit on the flake input path if it supports submodules.
-  src = fetchFromGitHub {
-    owner = "Julia-Roman";
-    repo = "rustlog";
-    rev = "master"; # or a specific commit hash
-    fetchSubmodules = true;
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # update this hash on first build
-  };
+  src = inputs.rustlog;
 
   cargoHash = "sha256-JYG+t9Cs6t55kW2kYE1jEUYEs3XvpzVSxIjbszkd4Sw=";
 
