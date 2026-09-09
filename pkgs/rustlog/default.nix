a0df1ae6005a3b3f2094cf9d64090401e808b786
itsjaylen <bossjaylen145@gmail.com>
2026-09-09 16:43:23 -0400

    Update default.nix

--- pkgs/rustlog/default.nix
+++ pkgs/rustlog/default.nix
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage {
 
   offlineCache = fetchYarnDeps {
     yarnLock = "${inputs.rustlog}/web/yarn.lock";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-tyqpeAI6hu0YlTWvZMJekMU7lIHEOv137KP+ci+Cv7k=";
   };
 
   nativeBuildInputs = [
