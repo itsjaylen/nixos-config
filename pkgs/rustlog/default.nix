f46ccb4d94499848547d9160d2250798edcdea2f
itsjaylen <bossjaylen145@gmail.com>
2026-09-10 00:16:55 -0400

    Update default.nix

--- pkgs/rustlog/default.nix
+++ pkgs/rustlog/default.nix
@@ -32,18 +32,15 @@ rustPlatform.buildRustPackage {
   ];
 
   preBuild = ''
-    cd web
-    export HOME=$(mktemp -d)
-    
-    # Configure yarn to use the offline cache properly
-    yarn config --offline set yarn-offline-mirror "$offlineCache"
-    
-    # Fix offline mirror permissions if necessary and install dependencies
-    fixup-yarn-lock yarn.lock || true
-    yarn install --offline --frozen-lockfile --no-progress --ignore-scripts
-    
-    # Build the frontend assets
-    node ./node_modules/vite/bin/vite.js build
-    cd ..
-  '';
+      export HOME=$(mktemp -d)
+      
+      # Configure yarn offline mirror
+      yarn config --offline set yarn-offline-mirror "$offlineCache"
+      
+      # Install dependencies and build vite from the web directory or root as appropriate
+      cd web
+      yarn install --offline --frozen-lockfile --no-progress --ignore-scripts
+      node ./node_modules/vite/bin/vite.js build
+      cd ..
+    '';
 }
