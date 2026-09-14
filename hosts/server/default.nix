cc8487372febc1521b34dc24567bc7574910373f
itsjaylen <bossjaylen145@gmail.com>
2026-09-14 18:37:20 -0400

    fix port

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -50,5 +50,15 @@
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.tmp.useTmpfs = lib.mkForce false;
 
-  networking.firewall.allowedTCPPorts = [ 30080 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 30080 ];
+    trustedInterfaces = [ "cni0" "flannel.1" ];
+    # Or more aggressively, allow the pod CIDR:
+    extraForwardRules = ''
+      iifname "cni0" accept
+      iifname "flannel.1" accept
+      ip saddr 10.42.0.0/16 accept
+      ip daddr 10.42.0.0/16 accept
+    '';
+  };
 }
