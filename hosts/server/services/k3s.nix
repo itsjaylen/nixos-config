6c08c1993b595544e1ac095dfeb14d661890758d
itsjaylen <bossjaylen145@gmail.com>
2026-09-14 10:03:28 -0400

    Update k3s.nix

--- hosts/server/services/k3s.nix
+++ hosts/server/services/k3s.nix
@@ -5,12 +5,14 @@
     6443 # API server
   ];
 
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = [
-    "--write-kubeconfig-mode 640"
-    "--write-kubeconfig-group k3sconfig"
-  ];
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = [
+      "--write-kubeconfig-mode 640"
+      "--write-kubeconfig-group k3sconfig"
+    ];
+  };
   
   users.groups.k3sconfig = {};
   users.users.jaylen.extraGroups = [ "k3sconfig" ];
