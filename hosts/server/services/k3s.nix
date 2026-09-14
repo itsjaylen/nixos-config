fd0b55188fac27c2238c5fb50b5bde37b2c9c97b
itsjaylen <bossjaylen145@gmail.com>
2026-09-14 09:45:26 -0400

    Update k3s.nix

--- hosts/server/services/k3s.nix
+++ hosts/server/services/k3s.nix
@@ -7,6 +7,13 @@
 
   services.k3s.enable = true;
   services.k3s.role = "server";
+  services.k3s.extraFlags = [
+    "--write-kubeconfig-mode 640"
+    "--write-kubeconfig-group k3sconfig"
+  ];
+  
+  users.groups.k3sconfig = {};
+  users.users.jaylen.extraGroups = [ "k3sconfig" ];
 
   environment.systemPackages = [ pkgs.k3s ];
 }
