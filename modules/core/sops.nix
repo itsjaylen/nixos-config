042e9f478babb914e27fe7008c8e4469d0aa246d
itsjaylen <bossjaylen145@gmail.com>
2026-09-15 18:21:21 -0400

    Update sops.nix

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -33,6 +33,7 @@
       "grafana_secret_key" = { };
       "gitea_runner_token" = { };
       "k3s/node-token" = { };
+      "postgres_exporter_env" = { ... };
       "postgres_exporter_password" = {
         owner = "prometheus-postgres-exporter";
         group = "prometheus-postgres-exporter";
