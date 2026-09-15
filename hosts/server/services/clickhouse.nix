fdf90b052abaa5064e6c9d704c545155900dd030
itsjaylen <bossjaylen145@gmail.com>
2026-09-15 19:20:18 -0400

    Update clickhouse.nix

--- hosts/server/services/clickhouse.nix
+++ hosts/server/services/clickhouse.nix
@@ -9,29 +9,32 @@
     };
   };
 
-  # Write the rustlog fragment into ClickHouse's users.d/ directory.
-  # preStart runs as root (PermissionsStartOnly is implied when preStart is set
-  # and no User override is present), so it can write to /etc.
-  systemd.services.clickhouse.preStart = ''
-    +mkdir -p /etc/clickhouse-server/users.d
-    +cat > /etc/clickhouse-server/users.d/rustlog.xml <<EOF
-    <clickhouse>
-      <users>
-        <rustlog>
-          <password_sha256_hex>$(cat /run/secrets/clickhouse_user_password_sha256)</password_sha256_hex>
-          <networks>
-            <ip>127.0.0.1</ip>
-            <ip>::1</ip>
-          </networks>
-          <profile>default</profile>
-          <quota>default</quota>
-        </rustlog>
-      </users>
-    </clickhouse>
-    EOF
-    +chown clickhouse:clickhouse /etc/clickhouse-server/users.d/rustlog.xml
-    +chmod 0400 /etc/clickhouse-server/users.d/rustlog.xml
-  '';
+  # Write the rustlog fragment into ClickHouse's users.d/ directory
+  # using a root-privileged ExecStartPre.
+  systemd.services.clickhouse.serviceConfig = {
+    ExecStartPre = [
+      "+${pkgs.writeShellScript "clickhouse-write-users" ''
+        mkdir -p /etc/clickhouse-server/users.d
+        cat > /etc/clickhouse-server/users.d/rustlog.xml <<EOF
+        <clickhouse>
+          <users>
+            <rustlog>
+              <password_sha256_hex>$(cat /run/secrets/clickhouse_user_password_sha256)</password_sha256_hex>
+              <networks>
+                <ip>127.0.0.1</ip>
+                <ip>::1</ip>
+              </networks>
+              <profile>default</profile>
+              <quota>default</quota>
+            </rustlog>
+          </users>
+        </clickhouse>
+        EOF
+        chown clickhouse:clickhouse /etc/clickhouse-server/users.d/rustlog.xml
+        chmod 0400 /etc/clickhouse-server/users.d/rustlog.xml
+      ''}"
+    ];
+  };
 
   networking.firewall.allowedTCPPorts = [ 8123 9000 ];
 
