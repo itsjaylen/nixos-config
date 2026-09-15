42ed78083e4a45dc33a7e81eb645f6d97012ae96
itsjaylen <bossjaylen145@gmail.com>
2026-09-14 21:19:23 -0400

    Update sops.nix

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -1,27 +1,49 @@
-secrets = {
-  "github_ssh_key" = {
-    owner = "jaylen";
-    group = "users";
-    path = "/home/jaylen/.ssh/id_ed25519";
-    mode = "0600";
+{ config, pkgs, lib, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    sops
+    age
+    ssh-to-age
+  ];
+
+  sops = {
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.generateKey = true;
+
+    secrets = {
+      "github_ssh_key" = {
+        owner = "jaylen";
+        group = "users";
+        path = "/home/jaylen/.ssh/id_ed25519";
+        mode = "0600";
+      };
+      "gitea_token" = {
+        owner = "jaylen";
+        group = "users";
+      };
+      "garage_rpc_secret" = { };
+      "garage_s3_access_key" = { };
+      "garage_s3_secret_key" = { };
+      "restic_password" = { };
+      "grafana_admin_password" = { };
+      "grafana_secret_key" = { };
+      "gitea_runner_token" = { };
+      "s3_secret_key" = { };
+    } // lib.optionalAttrs (builtins.hasAttr "rustlog" config.users.users) {
+      "rustlog/config" = {
+        owner = "rustlog";
+        group = "rustlog";
+        path = "/var/lib/rustlog/config.json";
+        mode = "0400";
+      };
+    };
   };
-  "gitea_token" = {
-    owner = "jaylen";
-    group = "users";
-  };
-  "garage_rpc_secret" = { };
-  "garage_s3_access_key" = { };
-  "garage_s3_secret_key" = { };
-  "restic_password" = { };
-  "grafana_admin_password" = { };
-  "grafana_secret_key" = { };
-  "gitea_runner_token" = { };
-  "s3_secret_key" = { };
-} // lib.optionalAttrs (builtins.hasAttr "rustlog" config.users.users) {
-  "rustlog/config" = {
-    owner = "rustlog";
-    group = "rustlog";
-    path = "/var/lib/rustlog/config.json";
-    mode = "0400";
-  };
-};
+
+  system.activationScripts.ensureSshDir = ''
+    install -d -m 700 -o jaylen -g users /home/jaylen/.ssh
+  '';
+}
