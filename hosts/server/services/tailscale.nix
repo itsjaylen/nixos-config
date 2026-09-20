d30bf84182d25a4d3ea070d07584f25c06a71b96
itsjaylen <bossjaylen145@gmail.com>
2026-09-20 18:39:05 -0400

    fix tailscale no internet

--- hosts/server/services/tailscale.nix
+++ hosts/server/services/tailscale.nix
@@ -2,6 +2,7 @@
 {
   services.tailscale.enable = true;
   services.tailscale.extraUpFlags = [ "--ssh" ];
+  services.tailscale.extraSetFlags = [ "--accept-dns=false" ];
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
