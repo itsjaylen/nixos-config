7e8da5d3d5ea8c1fc1b350717dcbca0036cce097
itsjaylen <bossjaylen145@gmail.com>
2026-09-20 18:44:10 -0400

    Update cobblemon.nix

--- hosts/server/services/minecraft/cobblemon.nix
+++ hosts/server/services/minecraft/cobblemon.nix
@@ -4,7 +4,7 @@ let
   # Your Gitea-hosted packwiz pack
   modpack = pkgs.fetchPackwizModpack {
     url = "http://192.168.50.188:3000/itsjaylen/cobbleverse/raw/branch/main/pack.toml";
-    packHash = "sha256-ogBm7fW4NahiWB8zufiAb+G9MeT7yfiP6xIYRvBLIaM=";
+    packHash = "sha256-ew4JxQeozcUekUMyoiHGzIm/eCQMYaSsBEPsZXEWRgs=";
   };
 in
 {
